edca7081c24dc11de682823d8357fa1a18c27056
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-01 20:59:43 +0200

    Domaci 2

---
+++ Predavanje_2/Domaci_2/script.js
@@ -0,0 +1,14 @@
+let shop = {
+	shopName: "Dolce & Gabbana",
+	products: ["Derby shoes", "Essentials Sunglasses", "Card holder", "Necklace"],
+	openingHours: "8:00",
+	closingHours: "18:00",
+};
+
+console.log(shop); // Ispisivanje celog objekta
+console.log(
+	shop.shopName,
+	shop.products[0],
+	shop.openingHours,
+	shop.closingHours
+);
