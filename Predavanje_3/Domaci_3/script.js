24b69a96c080405d1eebc2e206c52dc7e08acecd
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-02 00:35:00 +0200

    domaci 3

---
+++ Predavanje_3/Domaci_3/script.js
@@ -0,0 +1,10 @@
+let name = "admin";
+let password = "tajnasifra";
+
+if (name == "admin" && password == "tajnasifra") {
+	console.log("Dobrodosli nazad!");
+} else if (name == "toma" && password == "tajnaSifra") {
+	console.log("Dobrodosao Tomo");
+} else {
+	console.log("Pogresna lozinka ili ime!!!");
+}
