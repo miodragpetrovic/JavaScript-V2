4feac9d940cc2ea14b8b74354fe52ca8a468b168
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-01 23:42:40 +0200

    if statement

---
+++ Predavanje_3/script.js
@@ -0,0 +1,12 @@
+//IF-ovi, uslovi, conditional statements
+
+let proizvod = "hleb";
+let cena = 99;
+
+if (cena == 99) {
+	console.log("Hleb je skup!");
+}
+
+if (proizvod == "hleb") {
+	console.log("Nasli smo hleb!");
+}
