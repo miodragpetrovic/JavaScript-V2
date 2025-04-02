7e827c5c3b047b9a4c03db16757e604a0ea5f84e
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-02 20:34:17 +0200

    if vezbe

--- Predavanje_4/script.js
+++ Predavanje_4/script.js
@@ -1,7 +1,24 @@
 let name = "miodrag";
 
-if ((name = "miodrag")) {
+if (name == "miodrag") {
 	console.log("Pozdrav Miodraze!");
 } else {
 	console.log("Niste administrator");
+}
+
+let password = "tajnaSifra";
+if (password == "tajnaSifra") {
+	console.log("Uneli ste tacnu sifru!");
+} else {
+	console.log("Uneli ste pogresnu sifru!");
+}
+
+// Jednostavnije resenje sa manje code-a
+
+if (name == "miodrag" && password == "tajnaSifra") {
+	console.log("Korisnicko ime i lozinka su tacni! Dobrodosli!!!");
+} else {
+	console.log(
+		"Uneli ste pogresno korisnicko ime ili loziku. Pokustajte ponovo!"
+	);
 }
