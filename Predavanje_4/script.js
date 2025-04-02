707d6f8e5f08037af2f8669df890f57dc7d1da24
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-02 21:10:28 +0200

    array if check

--- Predavanje_4/script.js
+++ Predavanje_4/script.js
@@ -13,7 +13,7 @@ if (password == "tajnaSifra") {
 	console.log("Uneli ste pogresnu sifru!");
 }
 
-// Jednostavnije resenje sa manje code-a
+// Less code solution
 
 if (name == "miodrag" && password == "tajnaSifra") {
 	console.log("Korisnicko ime i lozinka su tacni! Dobrodosli!!!");
@@ -21,4 +21,36 @@ if (name == "miodrag" && password == "tajnaSifra") {
 	console.log(
 		"Uneli ste pogresno korisnicko ime ili loziku. Pokustajte ponovo!"
 	);
+}
+
+// New exercise
+// Create new variable userType and value admin
+
+let userType = "admin";
+
+if (userType == "admin" || userType == "moderator") {
+	console.log("Vi ste administrator.");
+} else {
+	console.log("Vi ste obican clan");
+}
+
+// Kako sa nizom vrsiti uporedjivanje
+
+//Nacin 1
+
+let allowedTypes = ["admin", "moderator"]; // allowedTypes je array
+
+if (allowedTypes.includes(userType)) {
+	console.log("1. Vi ste administrator!");
+} else {
+	console.log("1. Vi ste obican clanxx!");
+}
+
+// Nacin 2
+
+let ifCheck = allowedTypes.includes(userType);
+if (ifCheck == true) {
+	console.log("2. Vi ste administrator!");
+} else {
+	console.log("2. Vi ste obican clan!");
 }
