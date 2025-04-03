757d257a342136884eb694d061e8baa6baaa2304
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-03 15:34:30 +0200

    vezbe part 3

---
+++ Vezbe/vezbe_part_3.js
@@ -0,0 +1,8 @@
+/* Kreiraj promenljivu koja sadrži tvoje godine, a zatim napiši if-else strukturu koja ispisuje "Maloletan" 
+ako je broj godina manji od 18, inače ispisuje "Punoletan". */
+
+/* Kreiraj niz sa nekoliko različitih brojeva, napiši if-else strukturu koja proverava da li je zbir prvog i 
+poslednjeg broja u nizu veći od 50 i ispiši odgovarajuću poruku. */
+
+/* Kreiraj promenljivu koja sadrži ime nekog voća, a zatim napiši if-else strukturu koja ispisuje "Ovo je jabuka" 
+ako je vrednost "jabuka", "Ovo je banana" ako je vrednost "banana", i "Neko drugo voće" za sve ostale vrednosti. */
