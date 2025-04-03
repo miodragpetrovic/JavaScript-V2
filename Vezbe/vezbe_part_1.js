2c2a50a29ae00977b499360d4463bfd3490ff679
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-03 15:45:40 +0200

    vezba 1

---
+++ Vezbe/vezbe_part_1.js
@@ -0,0 +1,7 @@
+/* Napraviti 2 varijable za ime i godine, koristeci console.log 
+ispisati poruku "Pozdrav ja se zovem IME i imam X godina" */
+
+let name = "Miodrag";
+let year = 41;
+
+console.log("Pozdrav ja se zovem " + name + " i imam " + year + " godinu.");
---
+++ Vezbe/vezbe_part_2.js
@@ -0,0 +1,8 @@
+/* Napraviti prazan array koji se zove "names", pronaci nacin kako da 
+naknadno dodate 3 imena "marija", "petar" i "jovan" u taj array */
+
+/* Napraviti array "komponente" i ubaciti 3 komponente, "monitor", "tastatura", "mis", 
+pronaci nacin kako da obrisete sve elemente in arraya */
+
+/* Napraviti array "books" koji sadrzi 3 knjige, pronaci nacin kako da naknadno dodati 
+novu knjigu na prvo mesto */
