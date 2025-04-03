5d0de179945c743958494e2582b36fe02fd3ebf5
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-03 15:49:38 +0200

    vezbe_part_2

--- Vezbe/vezbe_part_2.js
+++ Vezbe/vezbe_part_2.js
@@ -8,5 +8,9 @@ console.log(names);
 /* Napraviti array "komponente" i ubaciti 3 komponente, "monitor", "tastatura", "mis", 
 pronaci nacin kako da obrisete sve elemente in arraya */
 
+let components = ["monitor", "tastatura", "mis"];
+components.splice(0, 3);
+console.log(components);
+
 /* Napraviti array "books" koji sadrzi 3 knjige, pronaci nacin kako da naknadno dodati 
 novu knjigu na prvo mesto */
