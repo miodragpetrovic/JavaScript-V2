93fe73e2db4a38f41666a2e4cfaf06b21969c122
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-03 20:22:37 +0200

    predavanje 5

---
+++ Predavanje_5/script.js
@@ -0,0 +1,24 @@
+let name = "Admin";
+name = name.toLowerCase(); // kovertuje u mala slova
+
+if (name == "admin") {
+	console.log("Pozdrav admine!");
+} else {
+	console.log("Vi niste administrator");
+}
+
+console.log(name[0]);
+
+if (name[0] == "a") {
+	console.log("Vase ime pocinje samoglasnikom");
+}
+
+// Petlje - Loops
+
+/* let cars = ["AUDI", "MERZEDES", "BMW", "OPEL", "RENAULT"];
+cars.push("FIAT");
+
+for (i = 0; i < cars.length; i++) {
+	console.log(cars[i]);
+}
+ */
