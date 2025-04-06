0de948ade456af134b17ce11952ca19fbccb4104
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-06 22:08:53 +0200

    vezba

--- Predavanje_6/script.js
+++ Predavanje_6/script.js
@@ -9,3 +9,25 @@ for (let product in products) {
 for (let i = 0; i < products.length; i++) {
 	console.log(products[i]);
 }
+
+let korpa = [
+	{
+		name: "hleb",
+		price: 99,
+		amount: 1,
+	},
+	{
+		name: "luk",
+		price: 100,
+		amount: 1,
+	},
+	{
+		name: "Mleko",
+		price: 250,
+		amount: 1,
+	},
+];
+
+for (let korpe in korpa) {
+	console.log(korpa[korpe].price);
+}
