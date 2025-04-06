94e9cadeb0abfedc28815658fe055638e0cee067
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-06 12:15:58 +0200

    domaci 5

--- Predavanje_5/Domaci_5/script.js
+++ Predavanje_5/Domaci_5/script.js
@@ -24,7 +24,7 @@ let carMake = [
 	"Jeep",
 ];
 
-// carMake[i] -> petlja [0] -> prvo slovo
+// carMake[i] -> prolazi kroz petlju [0] -> uzim prvo slovo
 
 for (let i = 0; i < carMake.length; i++) {
 	if (carMake[i][0].toLowerCase() === "a") {
