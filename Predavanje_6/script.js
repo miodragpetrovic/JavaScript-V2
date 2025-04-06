1301ef48ac141681fccdc343501f6963b36007e9
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-06 12:56:03 +0200

    predavanje 6

---
+++ Predavanje_6/script.js
@@ -0,0 +1,11 @@
+let products = ["Hleb", "Mleko", "Jogurt"];
+
+// FOR IN Petlja -> Kod for in petlje uvek promenljivu deklarisati kao let ili const
+for (let product in products) {
+	console.log(products[product]);
+}
+
+// FOR PETLJA
+for (let i = 0; i < products.length; i++) {
+	console.log(products[i]);
+}
