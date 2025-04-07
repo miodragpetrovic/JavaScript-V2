2d250f87e350c3839c4d0ee533bce266dbfb2956
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-07 21:36:15 +0200

    vezbe

---
+++ Vezbe/vezbe_part_6.js
@@ -0,0 +1,20 @@
+/* Kreiraj promenljivu grade koja prima ocenu unetu od strane korisnika putem prompt funkcije, 
+a zatim napiši if-else strukturu koja koristi alert da ispise "Excellent" za ocene iznad 90, 
+"Good" za ocene između 75 i 90, "Average" za ocene između 50 i 75, i "Fail" za ocene ispod 50. */
+
+/* Kreiraj niz shoppingList koji sadrži nekoliko artikala unetih od strane korisnika putem prompt 
+funkcije (koristi split(',') da bi razdvojio stvari u nizu), a zatim napiši for petlju koja prolazi 
+kroz niz i koristi switch strukturu da ispise specifične poruke za određene artikle (npr. "Buy fruits", "Buy vegetables") 
+koristeći alert za prikaz poruka. */
+
+/* Kreiraj promenljivu weather koja prima stanje vremena (npr. "sunny", "rainy", "cloudy") uneto od strane 
+korisnika putem prompt funkcije, a zatim napiši switch strukturu koja koristi alert da ispise poruku kao što je 
+"Wear sunglasses" za "sunny", "Take an umbrella" za "rainy", itd. */
+
+/* Kreiraj niz colors koji sadrži nekoliko boja unetih od strane korisnika putem prompt funkcije (koristi split(',') 
+da bi razdvojio boje), a zatim napiši for petlju koja prolazi kroz niz i koristi switch strukturu da ispise specifične 
+poruke za određene boje (npr. "Red is vibrant", "Blue is calm"). */
+
+/* Kreiraj promenljivu userName koja prima ime uneto od strane korisnika putem prompt funkcije, a zatim napiši if-else 
+strukturu koja koristi alert da ispise "Hello, [userName]!" ako je ime kraće od 5 karaktera, a "Welcome, [userName]!" 
+ako je ime duže ili jednako 5 karaktera. */
