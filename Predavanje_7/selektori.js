e930979dca865864a157cd597cd699d41a869ec6
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 14:47:02 +0200

    vezba

---
+++ Predavanje_7/selektori.js
@@ -0,0 +1,10 @@
+/*
+- Starija metoda
+- getElementById -> prosledite ID, dobijate element
+- getElementByClassName -> Prosledite klasu i dobijate array sa elementima
+
+
+- Novija metoda
+- querySelector -> "#name", ".name" -> vraca 1 element
+- querySelectorAll -> "#name", ".name" -> vraca array sa elementima
+*/
