f88a46355cb3253fba7c5f31d1b59727120df8c1
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 22:54:01 +0200

    Predavanje 8

---
+++ Predavanje_8/script.js
@@ -0,0 +1,3 @@
+document.querySelector("#title").style.color = "red";
+document.querySelector(".subtitle").style.color = "green";
+document.getElementsByClassName("subtitle2")[0].style.color = "blue"; // [0] oznacava prvi element
