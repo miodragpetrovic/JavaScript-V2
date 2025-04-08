df77fc323e03960b1b52cee1b2fc1107222ba777
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 23:14:04 +0200

    querySelector

--- Predavanje_8/script.js
+++ Predavanje_8/script.js
@@ -3,6 +3,10 @@ document.querySelector(".subtitle").style.color = "green";
 document.getElementsByClassName("subtitle2")[0].style.color = "blue"; // [0] oznacava prvi element
 
 let products = document.querySelectorAll(".product");
-for (let p in products) {
+/* for (let p in products) {
 	products[p].style.color = "red";
+} */
+
+for (let product of products) {
+	product.style.color = "red";
 }
