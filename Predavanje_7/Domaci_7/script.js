b068bf7e50a077d942fbc1c1a41743baa7c27d55
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 17:35:21 +0200

    domaci 7

---
+++ Predavanje_7/Domaci_7/script.js
@@ -0,0 +1,20 @@
+let ads = [
+	{
+		title: "Skoda octavia 2014",
+		price: 9950,
+		image:
+			"https://www.autocar.co.uk/sites/autocar.co.uk/files/skoda-octavia.jpg",
+	},
+	{
+		title: "Audi A4",
+		price: 8000,
+		image:
+			"https://s1.cdn.autoevolution.com/images/gallery/AUDI-A4-5237_23.jpg",
+	},
+];
+
+let listingPage = document.querySelector("#listing");
+
+for (let i = 0; i < ads.length; i++) {
+	listingPage.innerHTML = ads[i].title;
+}
