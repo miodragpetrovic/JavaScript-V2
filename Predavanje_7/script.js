7cf203918f27babdd2a47df2671f0643b8483caa
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 13:14:43 +0200

    DOM vezba

--- Predavanje_7/script.js
+++ Predavanje_7/script.js
@@ -4,4 +4,16 @@ nameColor.style.color = "orange";
 nameColor.style.fontSize = "x-large";
 nameColor.style.backgroundColor = "black";
 
+// Promena teksta u p tagu
+let myCity = "Cuprija";
+let showCity = document.getElementById("name");
+showCity.textContent = myCity;
+
+// Prompt unos iena i prikaz u marquee
+let yourName = prompt("Your name");
+document.getElementById("name").textContent = yourName;
+
+let myColor = prompt("Unesite boju pozadine");
+document.getElementById("name").style.backgroundColor = myColor;
+
 // Vezba
