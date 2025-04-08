7a537d17df441adb12924f46266afd1e5da42da4
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-08 23:02:23 +0200

    Predavanje 8

--- Predavanje_8/script.js
+++ Predavanje_8/script.js
@@ -1,3 +1,8 @@
 document.querySelector("#title").style.color = "red";
 document.querySelector(".subtitle").style.color = "green";
 document.getElementsByClassName("subtitle2")[0].style.color = "blue"; // [0] oznacava prvi element
+
+let products = document.querySelectorAll(".product");
+for (let p in products) {
+	products[p].style.color = "red";
+}
