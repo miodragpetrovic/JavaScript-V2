2869291870f63dff25ea93ad0c175803acb6da43
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-10 14:26:19 +0200

    dom

---
+++ Vezbe_DOM/script.js
@@ -0,0 +1,4 @@
+let divElement = document.createElement("h1"); // Novi element div
+divElement.textContent = "Hallo Miodrag"; // Ubacivanje teksta unutar div-a
+
+document.body.appendChild(divElement); // Ipsisivanje u html
