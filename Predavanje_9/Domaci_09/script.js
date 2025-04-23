a7a2db962c387c410c8e154d7e5f6d465a182f1b
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-04-23 23:36:17 +0200

    arreys

--- Predavanje_9/Domaci_09/script.js
+++ Predavanje_9/Domaci_09/script.js
@@ -21,6 +21,35 @@ let cities = [
 	"Kruševac",
 ];
 
+let propertyTypes = [
+	"Stan",
+	"Kuća",
+	"Poslovni prostor",
+	"Lokal",
+	"Magacin",
+	"Vikendica",
+	"Plac",
+	"Poljoprivredno zemljište",
+	"Garaža",
+	"Soba",
+	"Penthaus",
+	"Apartman",
+	"Zgrada",
+	"Hotel",
+	"Industrijski objekat",
+];
+
+let propertySizes = [
+	"do 30 m²",
+	"31–50 m²",
+	"51–70 m²",
+	"71–100 m²",
+	"101–150 m²",
+	"151–200 m²",
+	"201–300 m²",
+	"više od 300 m²",
+];
+
 let citiesSelector = document.querySelector("#citiesSelector");
 
 for (let city of cities) {
