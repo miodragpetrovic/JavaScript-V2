8cc915f47881cc9a27a3d3c7c1753e1cef5537b2
miodragpetrovic <mp.miodrag.petrovic@gmail.com>
2025-06-09 23:16:21 +0200

    js

--- Predavanje_10/script.js
+++ Predavanje_10/script.js
@@ -65,7 +65,7 @@ let realEstates = [
   { city: "Kruševac", typ: "Stan", price: 60000, size: 100 },
 	{ city: "Trstenik", typ: "Kuca", price: 75000, size: 125 },
 	{ city: "Zajecar", typ: null, price: 10000, size: 30 }, // null
-	{ city: "Novi Pazar", type: "Hotel", price: 300000}
+	{ city: "Novi Pazar", type: "Hotel", price: 300000, size: 500 }
 ];
 
 
