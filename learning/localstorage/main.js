778ecbf5d12d835dcede3477e23a2a0e053a2c39
miodrag.petrovic <mp.miodrag.petrovic@gmail.com>
2025-09-20 13:37:55 +0200

    js

--- learning/localstorage/main.js
+++ learning/localstorage/main.js
@@ -1,3 +1,8 @@
+// Jednostavan primer koriscenja localStorage objekta
 localStorage.setItem('name', 'Miodrag Petrovic');
+localStorage.setItem('age', '30');
 
 console.log(localStorage.getItem('name'));
+
+let fullName = document.getElementById('displayFullName');
+fullName.innerHTML = "My full name is " + localStorage.getItem('name') + " and I'm " + localStorage.getItem('age') + " years old.";
