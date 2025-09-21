a92439e7e07d43a39f43f826d174018319d94cb8
miodrag.petrovic <mp.miodrag.petrovic@gmail.com>
2025-09-21 21:58:13 +0200

    js

--- learning/localstorage/main.js
+++ learning/localstorage/main.js
@@ -7,4 +7,13 @@ console.log(localStorage.getItem('name'));
 let fullName = document.getElementById('displayFullName');
 fullName.innerHTML = "My full name is " + localStorage.getItem('name') + " and I'm " + localStorage.getItem('age') + " years old.";
 
+
+// Ubaci preko forme ime u localStorage
 let firstName = document.getElementById('firstName');
+let btn  = document.querySelector('btn');
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    localStorage.setItem('name', firstName.value);
+    fullName.innerHTML = "My full name is " + localStorage.getItem('name') + " and I'm " + localStorage.getItem('age') + " years old.";
+})
