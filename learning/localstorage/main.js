db72ce5496d1f7189da340082b539aa097a7df93
miodrag.petrovic <mp.miodrag.petrovic@gmail.com>
2025-09-21 21:56:36 +0200

    js

--- learning/localstorage/main.js
+++ learning/localstorage/main.js
@@ -7,3 +7,4 @@ console.log(localStorage.getItem('name'));
 let fullName = document.getElementById('displayFullName');
 fullName.innerHTML = "My full name is " + localStorage.getItem('name') + " and I'm " + localStorage.getItem('age') + " years old.";
 
+let firstName = document.getElementById('firstName');
